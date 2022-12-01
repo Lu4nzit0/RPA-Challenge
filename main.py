a21bbf573b97abdff6b87fd14e8bed5a833bbd9f
Lu4nzit0 <luancuba.lc@gmail.com>
2022-12-01 00:09:23 -0300

    Checking for the file's existence in the downloads folder and a function that waits for the element's HTML to be generated was included.

--- main.py
+++ main.py
@@ -2,20 +2,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import pandas as pd
+import os.path
+import os
 
 driver = webdriver.Chrome()
+driver.implicitly_wait(10)
 
 driver.get("https://rpachallenge.com/")
 
-import pandas as pd
 
-x = pd.read_excel(r"C:\Users\Luan\Downloads\challenge.xlsx", engine='openpyxl')
+def download():
+  driver.find_element(By.XPATH, (f"//a[@href='./assets/downloadFiles/challenge.xlsx']")).click()
+
+
+if os.path.exists(r'C:\Users\Luan\Downloads\challenge.xlsx'):
+  print("O arquivo já existe!")
+  os.remove(r'C:\Users\Luan\Downloads\challenge.xlsx')
+  download()
+  
+else:
+  print("O arquivo não existe!")
+  print("Realizando download..")
+  download() 
 
 def fill_in_fields(ng_reflect_name, dado):
   driver.find_element(By.XPATH, (f"//input[@ng-reflect-name='{ng_reflect_name}']")).send_keys(dado)
 
 driver.find_element(By.XPATH, ("//button[.='Start']")).click()
 
+x = pd.read_excel(r'C:\Users\Luan\Downloads\challenge.xlsx', engine='openpyxl')
 rounds = 0
 while rounds < 11:
 
@@ -33,27 +49,14 @@ while rounds < 11:
   fill_in_fields('labelRole', cargo)
   fill_in_fields('labelAddress', endereco)
   fill_in_fields('labelEmail', email)
-  fill_in_fields('labelPhone', str(numero_telefone))
-
-  time.sleep(0.5)
+  fill_in_fields('labelPhone', str(numero_telefone))  
 
   driver.find_element(By.XPATH, ("//input[@value='Submit']")).click()
 
-  rounds = rounds + 1
-  
-  
-        
+  rounds = rounds + 1    
+ 
 
-
-
-#  driver.find_element(By.ID_NAME, "input").send_keys("TESTE PYTHON" + Keys.ENTER)
-# def preenche_dados(campo, dado):
 # "//input[@ng-reflect-name='labelFirstName']"
 # Xpath =//tagname[@Attribute='value']
 
 
-#   def preenche_dados(campo, dado):
-#     for(rounds = 0; )
-    
-
-# print('Deu Certo!')
